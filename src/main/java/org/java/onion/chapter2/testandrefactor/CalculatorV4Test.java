a39cf7433964d5d0f052f87a9e96c4c5660ae2b7
tjddnjs0102 <rjf654@gmail.com>
2024-01-23 03:40:44 +0900

    CalculatorV4Test.java 주석으로 설명 추가

--- src/main/java/org/java/onion/chapter2/testandrefactor/CalculatorV4Test.java
+++ src/main/java/org/java/onion/chapter2/testandrefactor/CalculatorV4Test.java
@@ -10,6 +10,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /*
 assertEquals(기대값, 결과값);
+값이 일치하는 경우 초록창이 뜨면서 넘어가지만,
+일치하지 않는 경우 기대치와 실제 값을 나타내어 테스트 결과를 알려준다.
  */
 public class CalculatorV4Test {
     @Test
@@ -28,4 +30,14 @@ public class CalculatorV4Test {
 
 // 테스트를 진행하면서 junit가 존재하지 않는다는 에러를 만나면
 // Settings > Build, Excution, Deployment > Build Tools > Gradle에서
-// Build and run using과 Run tests using을 모두 IntelliJ IDEA로 바꿔준다! (Gradle로 되어있었음.. )
+// Build and run using과 Run tests using을 모두 IntelliJ IDEA로 바꿔준다! (Gradle로 되어있었다.)
+
+/*
+JUnit의 Assert 클래스에 있는 메서드들..
+- assertEquals()
+- assertTrue()
+- assertFalse()
+- assertNull()
+- assertNotNull()
+- assertArrayEquals()
+ */
