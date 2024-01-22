56c9b6370bc2e07188dd95f8a3f2af809f465577
tjddnjs0102 <rjf654@gmail.com>
2024-01-22 17:08:10 +0900

    CalculatorTestV3.java

---
+++ src/main/java/org/java/onion/chapter2/testandrefactor/CalculatorTestV3.java
@@ -0,0 +1,20 @@
+package org.java.onion.chapter2.testandrefactor;
+
+import org.junit.jupiter.api.Test;
+
+/*
+JUnit은 @TEST 어노테이션을 사용해 각각의 테스트 메소드를 독립적으로 실행할 수 있다.
+ */
+public class CalculatorTestV3 {
+    @Test
+    public void add() {
+        CalculatorV2 cal = new CalculatorV2();
+        System.out.println(cal.add(6,3));
+    }
+
+    @Test
+    public void subtract() {
+        CalculatorV2 cal = new CalculatorV2();
+        System.out.println(cal.subtract(6,3));
+    }
+}
