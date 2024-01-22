c407c28f74e0f4fd96a3581f9f8884ef5ae7afed
tjddnjs0102 <rjf654@gmail.com>
2024-01-22 15:53:49 +0900

    계산기 Basic v1 설명 주석추가

--- src/main/java/org/java/onion/chapter2/testandrefactor/CalculatorV1.java
+++ src/main/java/org/java/onion/chapter2/testandrefactor/CalculatorV1.java
@@ -1,5 +1,10 @@
 package org.java.onion.chapter2.testandrefactor;
 
+/*
+서비스를 담당하는 프로덕션 코드와 테스트 코드(main())가 같은 클래스에 있어서 문제이다.
+테스트 코드는 테스트 단계에서만 필요하기 때문에 서비스 시점에 같이 배포될 필요가 없다.
+ */
+
 public class CalculatorV1 {
     int add(int i, int j) {
         return i + j;
