92544655503a421147e7ce2fe93938f7d9dd948e
tjddnjs0102 <rjf654@gmail.com>
2024-01-25 03:25:53 +0900

    refactor: 검증 메서드 분리, 빈 문자열을 입력해도 0 반환.

--- src/main/java/org/java/onion/chapter2/real/StringCalculator.java
+++ src/main/java/org/java/onion/chapter2/real/StringCalculator.java
@@ -5,34 +5,32 @@ import java.util.Scanner;
 public class StringCalculator {
     // 요구사항: 전달하는 문자를 구분자로 분리한 후 각 숫자의 합을 구해 반환한다.
     int add(String text) {
-
-        if (text == null || text.isEmpty()) {
+        if (text == null || text.isEmpty() || text.equals(" ")) {
             return 0;
-        }
-
-
-        if (!text.matches(".*[,|:].*")) {
-            return -2;
         }
 
         String[] numbers = text.split(",|:");
 
         int sum = 0;
         for (String number : numbers) {
-            int num;
-            try {
-                num = Integer.parseInt(number.trim());
-            }catch (NumberFormatException e) {
-                return -1;
-            }
-            if (num < 0) { // 요구사항: 음수를 입력하는 경우 RuntimeException을 발생시킨다.
-                throw new RuntimeException("음수는 입력할 수 없습니다.");
-            }
+            sum += parseAndValidate(number);
+        }
+        return sum;
+    }
 
-            sum += num;
+    private int parseAndValidate(String number) {
+        int num;
+        try {
+            num = Integer.parseInt(number.trim());
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("유효하지 않은 숫자입니다.");
         }
 
-        return sum;
+        if (num < 0) {
+            throw new RuntimeException("음수는 입력할 수 없습니다.");
+        }
+
+        return num;
     }
 
 
@@ -43,16 +41,13 @@ public class StringCalculator {
             System.out.println("쉼표(,) 혹은 클론(:)으로 구분된 값을 입력하시오.");
             String input = scanner.nextLine();
             StringCalculator calculator = new StringCalculator();
-            int result = calculator.add(input);
-
-            if (result == -2) {
-                System.out.println("오류: 입력은 쉼표 혹은 클론으로 구분되어야 합니다. 다시 시도해주세요.");
-                continue;
+            try {
+                int result = calculator.add(input);
+                System.out.println("합계: " + result);
+                break;
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
             }
-
-            System.out.println("합계: " + result);
-            break;
-
         }
     }
 }
