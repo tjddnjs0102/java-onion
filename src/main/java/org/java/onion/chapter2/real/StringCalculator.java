41a6133fae80c648bf8f5c0472d93977b02983f8
tjddnjs0102 <rjf654@gmail.com>
2024-01-25 01:50:25 +0900

    feat: 쉼표로 구분되고 있는지 확인 후 아니라면 재안내

--- src/main/java/org/java/onion/chapter2/real/StringCalculator.java
+++ src/main/java/org/java/onion/chapter2/real/StringCalculator.java
@@ -4,17 +4,30 @@ import java.util.Scanner;
 
 public class StringCalculator {
     int add(String text) {
+        if (!text.contains(",")) {
+            return -1;
+        }
         return 0;
     }
 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("쉼표(,)로 구분된 값을 입력하시오.");
-        String input = scanner.nextLine();
-        StringCalculator calculator = new StringCalculator();
 
-        int result = calculator.add(input);
-        System.out.println("합계: " + result);
+        while (true) {
+            System.out.println("쉼표(,)로 구분된 값을 입력하시오.");
+            String input = scanner.nextLine();
+            StringCalculator calculator = new StringCalculator();
+            int result = calculator.add(input);
+
+            if (result == -1) {
+                System.out.println("오류: 입력은 쉼표로 구분되어야 합니다. 다시 시도해주세요.");
+                continue;
+            }
+
+            System.out.println("합계: " + result);
+            break;
+
+        }
     }
 }
