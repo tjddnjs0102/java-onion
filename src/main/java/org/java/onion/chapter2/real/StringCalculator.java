655b4f1f3994d9c74927bddad175e89912dc591c
tjddnjs0102 <rjf654@gmail.com>
2024-01-25 10:49:17 +0900

    커스텀 구분자 사용가능토록 수정 (CUSTOM_DELIMITER_PATTERN)

--- src/main/java/org/java/onion/chapter2/real/StringCalculator.java
+++ src/main/java/org/java/onion/chapter2/real/StringCalculator.java
@@ -1,8 +1,13 @@
 package org.java.onion.chapter2.real;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
+
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.+)\n(.*)");
+
     // 누군가 add 메서드를 봤을 때 어떤 역할인지 알기 쉽도록 만들기
     int add(String text) {
         if (isBlank(text)) {
@@ -13,13 +18,20 @@ public class StringCalculator {
         return sumNumbers(numbers);
     }
 
+
     private boolean isBlank(String text) {
         return text == null || text.isEmpty();
     }
 
     private String[] splitNumbers(String text) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        if (matcher.matches()) {
+            String delimiter = Pattern.quote(matcher.group(1));
+            return matcher.group(2).split(delimiter);
+        }
         return text.split(",|:");
     }
+
 
     private int sumNumbers(String[] numbers) {
         int sum = 0;
